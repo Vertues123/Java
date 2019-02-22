7906452e5ab5c3e08a30917bb9018fa64a8e9e37
Vertues123 <43211590+Vertues123@users.noreply.github.com>
2019-02-22 17:34:24 +0100

    Using classes Basics

---
+++ Klasy/src/com/company/Animal.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Animal {
+    private int size;
+
+    public void speak()
+    {
+        System.out.println(" ");
+    }
+    public void setSize(int size)
+    {
+        this.size =size;
+    }
+    public int getSize()
+    {
+        return size;
+    }
+}
